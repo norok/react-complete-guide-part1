408b89fb87dc325b2e4559f5faf4632327483a99
Ivan Lewaschiw <thexsonic@gmail.com>
2018-04-22 15:28:16 -0300

    Section 5 - Ejecting the scripts and modifying the generation of css classes for custom scoped css.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css'; // this transforms CSS code into an object
 import Person from './Person/Person';
 
 class App extends Component {
@@ -81,14 +81,14 @@ class App extends Component {
 
     let classes = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      classes.push(styles.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      classes.push(styles.bold);
     }
 
     return (
-      <div className="App">
+      <div className={styles.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <button style={buttonStyle}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import styles from './Person.css';
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <div className={styles.Person}>
             <h4 onClick={props.click}>I'm {props.name} and I am {props.age} years old!</h4>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
